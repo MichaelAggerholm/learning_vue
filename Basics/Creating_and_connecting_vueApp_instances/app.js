c56873092196e5924d79a68857717ea02b8918f4
ma <92aggerholm@gmail.com>
2022-04-04 16:42:05 +0200

    finished course about creating and connecting vue application instances

--- Basics/Creating_and_connecting_vueApp_instances/app.js
+++ Basics/Creating_and_connecting_vueApp_instances/app.js
@@ -4,9 +4,31 @@ const app = Vue.createApp({
     data() {
         return {
             courseGoal: 'Finish this course!',
+            vueLink: 'https://vuejs.org',
+            courseGoalA: 'Finish this course!',
+            courseGoalB: '<h3>Build amazing vue application!</h3>',
         };
-    }
+    },
+    methods: {
+        outputGoalA() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn vue!';
+            } else {
+                return 'Master vue!';
+            }
+        },
+        outputGoalB() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
+    },  
 });
 
 // Connect created view to section with id: user-goal
+// Can only be bound to one html id, but automaticly gets access to all child elemetns of that id.
 app.mount('#user-goal');
