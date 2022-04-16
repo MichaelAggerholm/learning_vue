97e8e53c0b6cb8b7494a18e3a432ed85ccf8981f
ma <92aggerholm@gmail.com>
2022-04-16 22:57:01 +0200

    completed first task, second task tomorrow.

--- Basics/assignment_4/app.js
+++ Basics/assignment_4/app.js
@@ -1,13 +1,32 @@
 const App = Vue.createApp({
-    data() {
-        return {
-            user1: false,
-            user2: false,
-        }
+  data() {
+    return {
+      classInput: "",
+      user1: false,
+      user2: false,
+    };
+  },
+  computed: {
+    user1Class() {
+        return { user1: this.user1 };
     },
-    methods: {
-        
+    user2Class() {
+        return { user2: this.user2 };
     },
+},
+  methods: {
+    setClass(event) {
+      this.classInput = event.target.value;
+      if (this.classInput === "user1") {
+        this.user1 = true;
+      } else if (this.classInput === "user2") {
+        this.user2 = true;
+      } else {
+        this.user1 = false;
+        this.user2 = false;
+      }
+    },
+  },
 });
 
-App.mount('#assignment');
+App.mount("#assignment");
