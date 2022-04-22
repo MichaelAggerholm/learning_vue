0898a88b8a14a55ed3832deef10aa1e5e74cfc3a
ma <92aggerholm@gmail.com>
2022-04-22 22:36:21 +0200

    learning key and click.stop

--- Rendering_Conditionals/app.js
+++ Rendering_Conditionals/app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
-      enteredGoalValue: '',
+      enteredGoalValue: "",
       goals: [],
     };
   },
@@ -9,7 +9,10 @@ const app = Vue.createApp({
     addGoal() {
       this.goals.push(this.enteredGoalValue);
     },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
