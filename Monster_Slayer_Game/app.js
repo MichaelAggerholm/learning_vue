cbf3b6905e8204c5f6616ceeb29b16c6585713a6
ma <92aggerholm@gmail.com>
2022-04-25 19:37:38 +0200

    Followed course to add healing and game result.

--- Monster_Slayer_Game/app.js
+++ Monster_Slayer_Game/app.js
@@ -1,35 +1,78 @@
 function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
-    data() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100,
-        }
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
     },
-    computed: {
-        monsterBarStyles() {
-            return {width: this.monsterHealth + '%'};
-        },
-        playerBarStyles() {
-            return {width: this.playerHealth + '%'};
-        }
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
     },
-    methods: {
-        attackMonster() {
-            // Skad et sted mellem 5 og 12 dps.
-            const attackValue = getRandomValue(5, 12);
-            this.monsterHealth -= attackValue;
-            this.attackPlayer();
-        },
-        attackPlayer() {
-            // Skad et sted mellem 8 og 15 dps.
-            const attackValue = getRandomValue(8, 15);
-            this.playerHealth -= attackValue;
-        },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     },
+  },
+  watch: {
+      playerHealth(value) {
+        if(value <= 0 && this.monsterHealth <= 0) {
+            // A draw.
+            this.winner = 'draw';
+        } else if(value <= 0) {
+            // Player lost.
+            this.winner = 'monster';
+        } 
+      },
+      monsterHealth(value) {
+        if(value <= 0 && this.playerHealth <= 0) {
+            // A draw.
+            this.winner = 'draw';
+        } else if(value <= 0) {
+            // Monster lost.
+            this.winner = 'player';
+        } 
+      },
+  },
+  methods: {
+    attackMonster() {
+      this.currentRound++;
+      // Skad et sted mellem 5 og 12 dps.
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      // Skad et sted mellem 8 og 15 dps.
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      // Skad et sted mellem 8 og 15 dps.
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+  },
 });
 
-app.mount('#game');
+app.mount("#game");
