a15ff3d9e028dcdbe19f7e0fa68d53f2758d3bd2
Michael <92aggerholm@gmail.com>
2022-05-03 22:13:30 +0200

    ways of adding template values to vue apps.

--- Vue_Behind_the_scenes/app.js
+++ Vue_Behind_the_scenes/app.js
@@ -16,3 +16,17 @@ const app = Vue.createApp({
 });
 
 app.mount("#app");
+
+const app2 = Vue.createApp({
+  template: `<p>{{ favoriteMeal }}</p>`,
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    };
+  },
+  methods: {
+
+  }
+});
+
+app2.mount("#app2");
